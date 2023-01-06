919d11a887dec9ed54a21deaaf66734ccdab94e6
ViharGandhi <97725643+ViharGandhi@users.noreply.github.com>
2023-01-07 04:15:22 +0530

    Update Will.sol

--- backend/contracts/Will.sol
+++ backend/contracts/Will.sol
@@ -18,14 +18,16 @@ contract Will
            string property_declartion;
            address[] nominies_addr;
            string[] nominies_name;
+           string contentid;
+           
        }
 
     Create_will[] create_will;
     mapping(address => Create_will)public Will_Link;
     uint  i = 0;//content ID;
-    function WillCreate(address _Owner,string memory _OwnerName,string memory _Property_type,string memory _property_description,string memory _property_declartion,address[] calldata _nominies_addr,string[] calldata _nominies_name)public
+    function WillCreate(address _Owner,string memory _OwnerName,string memory _Property_type,string memory _property_description,string memory _property_declartion,address[] calldata _nominies_addr,string[] calldata _nominies_name,string memory _contentid)public
     {
-        create_will.push(Create_will({Owner:_Owner,Owner_name:_OwnerName,property_type:_Property_type,property_description:_property_description,property_declartion: _property_declartion,nominies_addr:_nominies_addr,nominies_name:_nominies_name}));
+        create_will.push(Create_will({Owner:_Owner,Owner_name:_OwnerName,property_type:_Property_type,property_description:_property_description,property_declartion: _property_declartion,nominies_addr:_nominies_addr,nominies_name:_nominies_name,contentid:_contentid}));
         Will_Link[_Owner] = create_will[i];
         i++;
     }
