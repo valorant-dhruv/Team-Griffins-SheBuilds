308c2a5896807e10df87e081fa5818c4ea9bdb55
valorant-dhruv <78591597+valorant-dhruv@users.noreply.github.com>
2023-01-06 13:46:34 +0530

    Initial commit

---
+++ backend/contracts/Storehash.sol
@@ -0,0 +1,19 @@
+//This is the solidity smart contract that stores the hash of the document that is stored in the IPFS
+
+//SPDX-License-Identifier:MIT
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Storehash{
+    //Inside the smart contract once when the data is stored inside the ipfs we get the content id of that image
+
+    //Hence we create a mapping for it
+    mapping(address=>string) immutable public storefiles;
+
+    //This is the function that stores the files
+    function storefile(string cid) public returns(bytes32)
+    {
+        //Now that we got the content id of the particular file we encode
+        storefiles[msg.sender]=cid;
+    }
+}
