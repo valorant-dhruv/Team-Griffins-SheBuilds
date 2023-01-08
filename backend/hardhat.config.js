0e5454da9bb29b3da8f158dceea24021baf1f32d
valorant-dhruv <78591597+valorant-dhruv@users.noreply.github.com>
2023-01-08 14:48:50 +0530

    Update hardhat.config.js

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -14,7 +14,7 @@ module.exports = {
     polygon_mumbai: {
       url: "https://ancient-skilled-surf.matic-testnet.discover.quiknode.pro/7b4f309e88e3a92395e1cd3f53a127991cfc40b3/",
       accounts: [
-        "0c7fcfbf8330ec840cae0000ba1553f5bcd6070e51747c72c24412b5979fda8f",
+        "",
       ],
       gas: 2100000,
       gasPrice: 8000000000,
@@ -22,6 +22,6 @@ module.exports = {
   },
   //We have described this object so that we can verify our smart contract when it is deployed
   etherscan: {
-    apiKey: "VAEVU6YVG5VXBZDFXHP2NAK8XRWADATSA6",
+    apiKey: "",
   },
 };
